7bef87cc4b4fdd887fd2a59268af971b5e2fff34
nurulhfuru <nurul1800016114@webmail.uad.ac.id>
2020-07-29 00:54:55 +0900

    tekweblanjutuas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TambahDataComponent } from './tambah-data/tambah-data.component';
+import { LoginComponent } from './login/login.component';
+import { TodoComponent } from './todo/todo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TambahDataComponent,
+    LoginComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
